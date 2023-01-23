d78396b5416c591d84f075404a03e76332ea4545
brachagreenberg <brachag85@gmail.com>
2023-01-23 13:38:46 -0500

    made the num column nullable

--- 2 sample data/2 sample data.sql
+++ 2 sample data/2 sample data.sql
@@ -5,3 +5,7 @@ union select 'nose', 1
 union select 'mouth', 2
 union select 'teeth', 32
 union select 'toungue', 1
+
+--added hair... how do you store body parts that dont have a fixed number
+insert body(bodypart)
+select 'hair'
